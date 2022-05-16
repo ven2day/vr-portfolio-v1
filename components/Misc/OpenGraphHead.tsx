061338b1fde884f351aca1fcccce7c2af39f2fff
Ven <56719881+ven2day@users.noreply.github.com>
2022-05-16 07:52:23 +0200

    Title

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
-    <title>MK Venkat | Java Full Stack Developer</title>
+    <title>MK Venkat | Java FullStack Developer </title>
     <meta name="description" content="My personal solace place on web-earth." />
-    <meta property="og:title" content=" M K Venkat | Java Full Stack Developer" />
-    <meta property="og:site_name" content="M K Venkat" />
+    <meta property="og:title" content=" MK  Venkat | Java Developer" />
+    <meta property="og:site_name" content=" Mohan Kumar Venkat" />
     <meta property="og:url" content="https://ventoday.com/" />
     <meta
       property="og:description"
-      content="Ohh you found me?. Howdy! I am Mohan Kumar Venkat. Or you could call me Ven. That works too . . . I am a Software Engineer"
+      content="Ohh you found me?. Howdy! I am Mohan Kumar Venkat. Or you could call me KL. That works too . . . I am a Java FullStack Developer"
     />
     <meta property="og:type" content="profile" />
     <meta
